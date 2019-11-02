9061007e451e6d329be1a2bcf8f97471ae2dd25b
mi-ko-cute <thx-mail@worysolve.com>
2019-11-02 15:02:42 +0900

    外部ファイルの読み込み&実行

---
+++ excercise/index.js
@@ -0,0 +1,11 @@
+const math = require('./math.js');
+
+const add = math.add(1, 2);
+const sub = math.sub(3, 4);
+const mul = math.mul(5, 6);
+const div = math.div(7, 8);
+
+console.log('math.add(1, 2)', add);
+console.log('math.sub(3, 4)', sub);
+console.log('math.mul(5, 6)', mul);
+console.log('math.div(7, 8)', div);
